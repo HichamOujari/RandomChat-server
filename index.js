357d2bc603c94f210b298690583482a0294b199c
HichamOujari <hichamoujari99@gmail.com>
2020-12-04 10:01:56 +0100

    first deploy

--- index.js
+++ index.js
@@ -7,6 +7,9 @@ const io = require('socket.io')(http,{
       methods: ["GET", "POST"]
     }
   })
+app.get('/', function(req, res) {
+  res.send('hello world');
+});
 var count=0;
 var Rooms = [];
 function addRoom(id) {
